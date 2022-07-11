b2e16f1bc22878acdf94bd258f615e1ce601649e
Mauricio Mahmud Sanchez <mauxms1996@gmail.com>
2022-07-11 15:42:42 -0400

    Simple Functions

---
+++ README.md
@@ -0,0 +1,50 @@
+# PanelScroller
+
+A very simple JavaScript library for move around the page using the viewport height size. 
+
+## Install
+
+Using npm:
+```bash
+npm install panelscroller 
+```
+
+## Use 
+We define a panel as a compoent with height size as window viewport size. Then, a n-panel will be the (n-1)-th viewport height size after the start of the page.
+
+The simple way to go to some panel is
+```javascript
+const manager = new PanelScrollManager();
+
+manager.GoToPanel(0); // go to the first panel
+manager.GoToPanel(1); // go to the second panel
+manager.GoToPanel(2); // go to the third panel
+//...
+manager.GoToPanel(n); // go to the (n-1)-th panel
+```
+
+Set a default behavior
+```javascript
+const manager = new PanelScrollManager();
+
+manager.SetBehavior('instant');
+manager.SetBehavior('smooth');
+manager.SetBehavior('auto');
+```
+
+Using a specific behavior only one time
+```javascript
+const manager = new PanelScrollManager();
+
+manager.GoToPanelWithBehavior(1, 'instant');
+manager.GoToPanelWithBehavior(1, 'smooth');
+manager.GoToPanelWithBehavior(1, 'auto');
+```
+
+You can get the deepest reached panel, this is calculated every time the `scroll` event is detected.
+```javascript
+const manager = new PanelScrollManager();
+
+manager.SetPanelCalculation(); // Activate this feature.
+manager.DeepestReachedPanel() // Get the panel number.
+```
---
+++ panelscroller.js
@@ -0,0 +1,61 @@
+class PanelScrollManager{
+
+	constructor(){
+		this._behavior = 'smooth';
+		this._maxTraveledPanel = 0;
+		this._isCalculationActive = false;
+	}
+
+	// Current panel. 	
+	get CurrentPanel(){
+		let vh =  window.visualViewport.height / document.documentElement.scrollHeight;
+		let currentPos = document.documentElement.scrollTop; 
+		let newPanel=Math.floor(currentPos / vh / document.documentElement.scrollHeight);
+
+		return newPanel;
+	}
+
+	// The deepest reached panel.  
+	get DeepestReachedPanel(){
+		if( !this._isCalculationActive )
+			console.error("The calculation of the deepest panel is not active. Turn it on with `SetPanelCalculation()`");	
+		return this._maxTraveledPanel;
+	}
+
+	// Go to the panel `index`, the first panel is 0.
+	GoToPanel(index){
+		let vh = window.visualViewport.height / document.documentElement.scrollHeight;
+		let currentPos =index * vh * document.documentElement.scrollHeight;
+
+		window.scrollTo({
+			top:currentPos,
+			behavior: this._behavior
+		})
+	}
+
+	// Got to the panel `index`, the first panel is 0. You can use 'smooth', 'instant', 'auto' for behavior, default is 'smooth'.
+	GoToPanelWithBehavior(index,behavior){
+		let temp = this._behavior;
+		this.SetBehavior(behavior);
+		this.GoToPanel(index);	
+		this.SetBehavior(temp);
+	}
+
+
+	// Active it for, get in any moment the deepest panel with `MaxTraveledPanel`
+	SetPanelCalculation(){
+		let self = this;
+		addEventListener('scroll',function(){
+			self._maxTraveledPanel = Math.max(self.CurrentPanel, self._maxTraveledPanel);
+			self._isCalculationActive = true;
+		});
+	}
+
+	// Set a behavior. You can use 'smooth', 'instant' or 'auto', default is 'smooth'
+	SetBehavior(behavior){
+		this._behavior = behavior;
+	}
+}
+
+
+export { PanelScrollManager };
